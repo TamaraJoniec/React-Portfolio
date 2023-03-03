73e44c2047952997b261e002bb0f18828bdd3b75
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-03 17:51:33 +0000

    Define project gallery component, use Bootstrap to render project gallery grid

--- assets/src/components/pages/ProjectGallery.js
+++ assets/src/components/pages/ProjectGallery.js
@@ -1,16 +1,41 @@
+// Import required modules
 import React from "react";
+import PropTypes from "prop-types";
+import { Col, Container, Row } from "react-bootstrap"; // Import the required Bootstrap components
 
-function ProjectGallery() {
-  return (
-    <div>
-      <h1 className="text-center">Project Gallery</h1>
-      <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii partiendo sed ne, no
-        mutat ludus aperiri mea, per in choro dolorem electram. Invidunt reprimique assueverit quo
-        ne, eruditi graecis pro ut. Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
-      </p>
-    </div>
-  );
+
+// Define project gallery component
+function ProjectGallery(props) {
+
+  // Render project gallery grid
+  <Container>
+      {/* Use a Bootstrap container to wrap the gallery */}
+      <Row xs={1} sm={2} md={3} className="g-4">
+        {/* Use a Bootstrap row to create a responsive grid of columns */}
+        {props.projects.map((project) => (
+          <Col key={project.id}>
+            {/* Use a Bootstrap col for each project */}
+            <div className="card h-100">
+              {/* Use a Bootstrap card component to display the project */}
+              <img
+                src={project.imageUrl}
+                className="card-img-top"
+                alt={project.title}
+              />
+              {/* Use a Bootstrap image component for the project */}
+              <div className="card-body">
+                {/* Use a Bootstrap card body to display the project title and description */}
+                <h5 className="card-title">{project.title}</h5>
+                <p className="card-text">{project.description}</p>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
 }
+
+
+// Define prop types for ProjectGallery component
 
 export default ProjectGallery;
