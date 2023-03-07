b5eefce25230bbd642274cabf46a389d2d2e0040
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-07 11:16:41 +0000

    styling for home-page hero banner added

--- assets/src/components/pages/Home.js
+++ assets/src/components/pages/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="home-container">
+      <Container fluid className="gradient-background py-5">
+        <Row>
+          <Col>
+            <h1 className="text-center text-light mb-4">Welcome</h1>
+            <h2 className="text-center text-light mb-5">Explore my portfolio and discover web design from my perspective.</h2>
+            <div className="text-center">
+              <button className="btn btn-lg btn-secondary mx-2">View Portfolio</button>
+              <button className="btn btn-lg btn-outline-light mx-2">Contact Me</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
