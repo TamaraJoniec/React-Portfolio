876149be500521beeec7c088a7a504f554faaab8
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-08-17 08:19:27 +0100

    image url for project 6

--- src/components/pages/ProjectsData.js
+++ src/components/pages/ProjectsData.js
@@ -43,7 +43,7 @@ const projectsData = [
       id: "6",
       title: "Marketing agency",
       description: "Scouting and refactoring code from a marketing agency website. ",
-      imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/blob/main/assets/images/01-html-css-git-challenge-demo.jpeg",
+      imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/01-html-css-git-challenge-demo.jpeg",
       deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
       githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
     },
