29ca51f9695ffece3c1100125d53fe082943ed1a
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-08-17 08:53:08 +0100

    updated projects section

--- src/components/pages/ProjectsData.js
+++ src/components/pages/ProjectsData.js
@@ -1,11 +1,11 @@
 const projectsData = [
     {
-      id: "1",
-      title: "Weather Dashboard",
-      description: "A weather dashboard with form inputs, where a user searches for a city and they are presented with current and future conditions for that city.",
-      imageUrl: "https://raw.githubusercontent.com/TamaraJoniec/Server-APIs-Weather-Dashboard/main/assets/images/10-server-side-apis-challenge-demo.png",
-      deployedUrl: "https://tamarajoniec.github.io/Server-APIs-Weather-Dashboard/",
-      githubUrl: "https://github.com/TamaraJoniec/Server-APIs-Weather-Dashboard",
+     
+      title: "Clicky-game",
+      description: "Clicky Game is a memory-challenging game built with React. The primary aim of the game is to test and enhance your memory. Click on an image to earn points, but be careful not to click on any image more than once!",
+      imageUrl: "https://github.com/TamaraJoniec/memory-game/raw/main/src/Screenshot.png",
+      deployedUrl: "https://clicky-game.netlify.app/",
+      githubUrl: "https://github.com/TamaraJoniec/memory-game",
     },
     {
       id: "2",
@@ -17,19 +17,20 @@ const projectsData = [
     },
     {
       id: "3",
-      title: "Clicky-game",
-      description: "Clicky Game is a memory-challenging game built with React. The primary aim of the game is to test and enhance your memory. Click on an image to earn points, but be careful not to click on any image more than once!",
-      imageUrl: "https://github.com/TamaraJoniec/memory-game/raw/main/src/Screenshot.png",
-      deployedUrl: "https://clicky-game.netlify.app/",
-      githubUrl: "https://github.com/TamaraJoniec/memory-game",
+      id: "1",
+      title: "Weather Dashboard",
+      description: "A weather dashboard with form inputs, where a user searches for a city and they are presented with current and future conditions for that city.",
+      imageUrl: "https://raw.githubusercontent.com/TamaraJoniec/Server-APIs-Weather-Dashboard/main/assets/images/10-server-side-apis-challenge-demo.png",
+      deployedUrl: "https://tamarajoniec.github.io/Server-APIs-Weather-Dashboard/",
+      githubUrl: "https://github.com/TamaraJoniec/Server-APIs-Weather-Dashboard",
     },
     {
       id: "4",
-      title: "Bootstrap Portfolio",
-      description: "A portfolio created using Bootstrap. ",
-      imageUrl: "https://github.com/TamaraJoniec/Bootstrap-Portfolio/raw/main/assets/images/screencapture.png",
-      deployedUrl: "https://tamarajoniec.github.io/Bootstrap-Portfolio/",
-      githubUrl: "https://github.com/TamaraJoniec/Bootstrap-Portfolio",
+      title: "Marketing agency",
+      description: "Scouting and refactoring code from a marketing agency website. ",
+      imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/01-html-css-git-challenge-demo.jpeg",
+      deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
+      githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
     },
     {
       id: "5",
@@ -39,14 +40,14 @@ const projectsData = [
       deployedUrl: "https://tamarajoniec.github.io/Furniture-Website/",
       githubUrl: "https://github.com/TamaraJoniec/Furniture-Website",
     },
-    {
-      id: "6",
-      title: "Marketing agency",
-      description: "Scouting and refactoring code from a marketing agency website. ",
-      imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/01-html-css-git-challenge-demo.jpeg",
-      deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
-      githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
-    },
+    // {
+    //   id: "6",
+    //   title: "Marketing agency",
+    //   description: "Scouting and refactoring code from a marketing agency website. ",
+    //   imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/01-html-css-git-challenge-demo.jpeg",
+    //   deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
+    //   githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
+    // },
   ];
   
   export default projectsData;
