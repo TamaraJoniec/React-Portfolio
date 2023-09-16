388c20940caac496705093b9611ece881554f0ae
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-09-16 13:45:01 +0100

    changed menu button to a toggle icon and downloaded bootstrap

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function NavTabs() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
     <div className="navbar">
-      <button id="toggleNav" onClick={() => setNavOpen(!navOpen)}>Menu</button>
-      <ul style={{ transform: navOpen ? 'translateX(0)' : 'translateX(100%)' }}>
-        <li>
-          <button onClick={() => setNavOpen(false)}>X</button>
-        </li>
-        <li><a href="/React-Portfolio/">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="/React-Portfolio/project-gallery/">Portfolio</a></li>
-        <li><a href="/React-Portfolio/contact/">Contact</a></li>
-      </ul>
-
+      <button className="hamburger" onClick={() => setNavOpen(!navOpen)}>
+        <i className="bi bi-list"></i>
+        <ul style={{ transform: navOpen ? 'translateX(0)' : 'translateX(100%)' }}>
+          <li>
+            <button onClick={() => setNavOpen(false)}>X</button>
+          </li>
+          <li><a href="/React-Portfolio/">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="/React-Portfolio/project-gallery/">Portfolio</a></li>
+          <li><a href="/React-Portfolio/contact/">Contact</a></li>
+        </ul>
+      </button>
     </div>
 
   );
