d90381fffb8e9a56867a1875b02692d274f1a734
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-09-17 13:46:11 +0100

    updated footer

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -13,7 +13,6 @@ function ProjectGallery(props) {
   }, []);
 
   return (
-    <>
     <Container className="project-gallery-container">
       <h2 className="gallery-title">Past Projects</h2>
       <Row xs={1} sm={2} md={3} className="g-4">
@@ -57,10 +56,9 @@ function ProjectGallery(props) {
       <br />
       <br />
     </Container>
-    <Footer />
-    </>
   );
 }
+
 
 
 export default ProjectGallery;
