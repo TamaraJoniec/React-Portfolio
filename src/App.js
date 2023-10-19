4c22db727a1f154deaadc7c9dd522dc721abce98
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-10-19 17:00:42 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
         <Routes basename="/">
           {/* Define routes using the Route component to render different page components at different paths */}
           {/* Define a default route that will render the Home component */}
-          <Route path="/" element={<Home />} />
-          <Route path="/project-gallery/" element={<ProjectGallery projects={projectsData} />} />
+          <Route path="/React-Portfolio/" element={<Home />} />
+          <Route path="/React-Portfolio/project-gallery/" element={<ProjectGallery projects={projectsData} />} />
           {/* Define a route that will have descendant routes */}
-          <Route path="/contact/" element={<Contact />} />
+          <Route path="/React-Portfolio/contact/" element={<Contact />} />
         </Routes>
       </div>
     </Router>
