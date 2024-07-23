d20466721f4a0e01da7706790c8f40f58fd95651
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2024-07-23 14:33:49 +0100

    Remove comments

--- src/App.js
+++ src/App.js
@@ -12,13 +12,9 @@ function App() {
     <Router basename="/">
       <div>
         <NavTabs />
-        {/* Wrap Route elements in a Routes component */}
         <Routes>
-          {/* Define routes using the Route component to render different page components at different paths */}
-          {/* Define a default route that will render the Home component */}
           <Route path="/" element={<Home />} />
           <Route path="/project-gallery/" element={<ProjectGallery projects={projectsData} />} />
-          {/* Define a route that will have descendant routes */}
           <Route path="/contact/" element={<Contact />} />
         </Routes>
       </div>
