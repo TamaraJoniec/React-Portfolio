04450272b68f68dd5dbb969bf2e2b5e41ea853f5
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2024-07-23 14:31:28 +0100

    Add aria-labels in contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,40 +1,84 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
   return (
     <div>
       <div className="contact-container">
         <div className="row">
-          <div className="contact-info text-white col-lg-6 mb-5">
+          <address className="contact-info text-white col-lg-6 mb-5">
             <h1 className="display-4 mb-4">Contact info</h1>
             <h2>Hello, I'm</h2>
             <h1>Tamara Joniec</h1>
-            {/* React icons instead of text for the links */}
             <ul className="list-unstyled lead">
               <li className="my-2">
-                <a href="https://github.com/TamaraJoniec" target="_blank" rel="noreferrer" className="text-decoration-none text-primary" style={{ display: 'flex', alignItems: 'center' }}>
-                  <FontAwesomeIcon icon={faGithub} className="link me-2" style={{ fontSize: '24px', color: 'white' }} /><span style={{ fontSize: '20px', color: 'white' }}>GitHub Profile</span>
+                <a
+                  href="https://github.com/TamaraJoniec"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-decoration-none text-primary"
+                  style={{ display: "flex", alignItems: "center" }}
+                  aria-label="GitHub Profile (opens in new window)">
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="link me-2"
+                    style={{ fontSize: "24px", color: "white" }}
+                    aria-hidden="true"
+                  />
+                  <span className="visually-hidden">GitHub Profile</span>
+                  <span style={{ fontSize: "20px", color: "white" }}>
+                    GitHub Profile
+                  </span>
                 </a>
               </li>
               <li className="my-2">
-                <a href="https://www.linkedin.com/in/tamara-joniec/" rel="noreferrer" target="_blank" className="text-decoration-none text-primary" style={{ display: 'flex', alignItems: 'center' }}>
-                  <FontAwesomeIcon icon={faLinkedin} className="link me-2" style={{ fontSize: '24px', color: 'white' }} /><span style={{ fontSize: '20px', color: 'white' }}>LinkedIn Page</span>
+                <a
+                  href="https://www.linkedin.com/in/tamara-joniec/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-decoration-none text-primary"
+                  style={{ display: "flex", alignItems: "center" }}
+                  aria-label="LinkedIn Page (opens in new window)">
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    className="link me-2"
+                    style={{ fontSize: "24px", color: "white" }}
+                    aria-hidden="true"
+                  />
+                  <span className="visually-hidden">LinkedIn Page</span>
+                  <span style={{ fontSize: "20px", color: "white" }}>
+                    LinkedIn Page
+                  </span>
                 </a>
               </li>
               <li className="my-2">
-                <a href="https://drive.google.com/file/d/1NPU7LhEdwc5OcASECj24n3-6mVRregbs/view?usp=share_link" rel="noreferrer" download className="text-decoration-none text-primary" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-                  <FontAwesomeIcon icon={faFilePdf} className="link me-2" style={{ fontSize: '24px', color: 'white' }} /><span style={{ fontSize: '20px', color: 'white' }}>Download Resume (PDF)</span>
+                <a
+                  href="https://drive.google.com/file/d/1NPU7LhEdwc5OcASECj24n3-6mVRregbs/view?usp=share_link"
+                  rel="noreferrer"
+                  download
+                  target="_blank"
+                  className="text-decoration-none text-primary"
+                  style={{ display: "flex", alignItems: "center" }}
+                  aria-label="Download CV (opens in new window)">
+                  <FontAwesomeIcon
+                    icon={faFilePdf}
+                    className="link me-2"
+                    style={{ fontSize: "24px", color: "white" }}
+                    aria-hidden="true"
+                  />
+                  <span className="visually-hidden">Download CV</span>
+                  <span style={{ fontSize: "20px", color: "white" }}>
+                    Download CV
+                  </span>
                 </a>
               </li>
             </ul>
-          </div>
+          </address>
         </div>
       </div>
     </div>
-
   );
 }
 
