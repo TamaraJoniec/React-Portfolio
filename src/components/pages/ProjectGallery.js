418762f0215b3d527e0d6b384231b3c82b479a50
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2024-07-23 17:18:40 +0100

    Add Numiko projects

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -1,6 +1,10 @@
 // Import required modules
 import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import edenProjectImage from "./images/eden-project.jpg";
+import chathamHouseImage from "./images/chatham-house.png";
+import jrfImage from "./images/jrf.png";
+
 import "aos/dist/aos.css";
 
 function ProjectGallery(props) {
@@ -14,10 +18,82 @@ function ProjectGallery(props) {
   return (
     <section aria-labelledby="gallery-title">
       <Container id="project-gallery" className="project-gallery-container">
+        <Row className="justify-content-center">
+          <Col md={8} className="text-center">
+            <h2 id="gallery-title" className="gallery-title">
+              Recent projects at Numiko
+            </h2>
+          </Col>
+          <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+            <article className="card h-100 mt-4">
+              <img
+                src={edenProjectImage}
+                className="card-img-top"
+                alt="Eden Project Booking Engine"
+              />
+              <div className="card-body">
+                <h3 className="card-title">Eden Project Booking Engine</h3>
+                <div className="btn-container">
+                  <a
+                    href="https://www.edenproject.com/visit/book-tickets"
+                    className="btn btn-visit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Visit Eden Project (opens in new window)"
+                  >
+                    Visit Site
+                  </a>
+                </div>
+              </div>
+            </article>
+            <article className="card h-100 mt-4">
+              <img
+                src={chathamHouseImage}
+                className="card-img-top"
+                alt="Chatham House sliders"
+              />
+              <div className="card-body">
+                <h3 className="card-title">Chatham House | Sliders</h3>
+                <div className="btn-container">
+                  <a
+                    href="https://www.chathamhouse.org/"
+                    className="btn btn-visit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Visit Chatham House (opens in new window)"
+                  >
+                    Visit Site
+                  </a>
+                </div>
+              </div>
+            </article>
+            <article className="card h-100 mt-4">
+              <img
+                src={jrfImage}
+                className="card-img-top"
+                alt="Joseph Rowntree Foundation"
+              />
+              <div className="card-body">
+                <h3 className="card-title">Joseph Rowntree Foundation</h3>
+                <div className="btn-container">
+                  <a
+                    href="https://www.jrf.org.uk/"
+                    className="btn btn-visit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Visit Joseph Rowntree Foundation (opens in new window)"
+                  >
+                    Visit Site
+                  </a>
+                </div>
+              </div>
+            </article>
+          </Row>
+        </Row>
         <h2 id="gallery-title" className="gallery-title">
           Past Projects
         </h2>
-        <Row xs={1} sm={1} md={2} className="g-4">
+        <Row xs={1} sm={1} md={2} lg={3} className="g-4">
           {props.projects ? (
             props.projects.map((project) => (
               <Col key={project.id}>
@@ -25,7 +101,8 @@ function ProjectGallery(props) {
                   <img
                     src={project.imageUrl}
                     className="card-img-top"
-                    alt={project.title} />
+                    alt={project.title}
+                  />
                   <div className="card-body">
                     <h3 className="card-title">{project.title}</h3>
                     <div className="btn-container">
@@ -34,7 +111,8 @@ function ProjectGallery(props) {
                         className="btn btn-visit"
                         target="_blank"
                         rel="noopener noreferrer"
-                        aria-label={`Visit ${project.title} (opens in new window)`}>
+                        aria-label={`Visit ${project.title} (opens in new window)`}
+                      >
                         Visit Site
                       </a>
                       {project.githubUrl && (
--- src/components/pages/ProjectsData.js
+++ src/components/pages/ProjectsData.js
@@ -1,39 +1,43 @@
 const projectsData = [
-    {
-      id: "1",
-      title: "Meeow",
-      imageUrl: "https://github.com/TamaraJoniec/Meeow/raw/main/assets/ScreenshotMeeow.png",
-      deployedUrl: "https://www.meeow.com/",
-      githubUrl: "https://github.com/TamaraJoniec/Meeow" ,
-    },
-    {
-      id: "2",
-      title: "Marketing agency",
-      imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/screenrecording.gif",
-      deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
-      githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
-    },
-    {
-      id: "3",
-      title: "Furniture store",
-      imageUrl: "https://github.com/TamaraJoniec/Furniture-Website/raw/main/images/Screenshot.png",
-      deployedUrl: "https://tamarajoniec.github.io/Furniture-Website/",
-      githubUrl: "https://github.com/TamaraJoniec/Furniture-Website",
-    },
-    {
-      id: "4",
-      title: "Password generator",
-      imageUrl: "https://github.com/TamaraJoniec/Password-Generator/raw/main/assets/images/Screenshot.png",
-      deployedUrl: "https://tamarajoniec.github.io/Password-Generator/",
-      githubUrl: "https://github.com/TamaraJoniec/Password-Generator/tree/main",
-    },
-    {
-      id: "5",
-      title: "Visit Leeds",
-      imageUrl: "https://github.com/TamaraJoniec/JumpLinks/raw/main/src/demo.gif",
-      deployedUrl: "https://jump-links.vercel.app/",
-      githubUrl: "https://github.com/TamaraJoniec/JumpLinks/tree/main",
-    },
-  ];
-  
-  export default projectsData;
+  {
+    id: "1",
+    title: "Meeow",
+    imageUrl:
+      "https://github.com/TamaraJoniec/Meeow/raw/main/assets/ScreenshotMeeow.png",
+    deployedUrl: "https://www.meeow.com/",
+    githubUrl: "https://github.com/TamaraJoniec/Meeow",
+  },
+  {
+    id: "2",
+    title: "Marketing agency",
+    imageUrl:
+      "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/screenrecording.gif",
+    deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
+    githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
+  },
+  {
+    id: "3",
+    title: "Furniture store",
+    imageUrl:
+      "https://github.com/TamaraJoniec/Furniture-Website/raw/main/images/Screenshot.png",
+    deployedUrl: "https://tamarajoniec.github.io/Furniture-Website/",
+    githubUrl: "https://github.com/TamaraJoniec/Furniture-Website",
+  },
+  {
+    id: "4",
+    title: "Password generator",
+    imageUrl:
+      "https://github.com/TamaraJoniec/Password-Generator/raw/main/assets/images/Screenshot.png",
+    deployedUrl: "https://tamarajoniec.github.io/Password-Generator/",
+    githubUrl: "https://github.com/TamaraJoniec/Password-Generator/tree/main",
+  },
+  {
+    id: "5",
+    title: "Visit Leeds",
+    imageUrl: "https://github.com/TamaraJoniec/JumpLinks/raw/main/src/demo.gif",
+    deployedUrl: "https://jump-links.vercel.app/",
+    githubUrl: "https://github.com/TamaraJoniec/JumpLinks/tree/main",
+  },
+];
+
+export default projectsData;
