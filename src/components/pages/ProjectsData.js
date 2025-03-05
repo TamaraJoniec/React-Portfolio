88ae1c9523757b8ae74c746a7a3915db00ec1905
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2025-03-05 13:59:20 +0000

    Add github-user-finder project

--- src/components/pages/ProjectsData.js
+++ src/components/pages/ProjectsData.js
@@ -2,40 +2,35 @@ const projectsData = [
   {
     id: "0",
     title: "Weather App",
-    imageUrl:
-      "https://github.com/TamaraJoniec/Server-APIs-Weather-Dashboard/raw/main/assets/images/Screenshot.png",
+    imageUrl: "https://github.com/TamaraJoniec/Server-APIs-Weather-Dashboard/raw/main/assets/images/Screenshot.png",
     deployedUrl: "https://tamarajoniec.github.io/Server-APIs-Weather-Dashboard/",
     githubUrl: "https://github.com/TamaraJoniec/Server-APIs-Weather-Dashboard",
   },
   {
     id: "1",
     title: "Meeow",
-    imageUrl:
-      "https://github.com/TamaraJoniec/Meeow/raw/main/assets/ScreenshotMeeow.png",
+    imageUrl: "https://github.com/TamaraJoniec/Meeow/raw/main/assets/ScreenshotMeeow.png",
     deployedUrl: "https://www.meeow.com/",
     githubUrl: "https://github.com/TamaraJoniec/Meeow",
   },
   {
     id: "2",
     title: "Marketing agency",
-    imageUrl:
-      "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/screenrecording.gif",
+    imageUrl: "https://github.com/TamaraJoniec/Code-Refactor/raw/main/assets/images/screenrecording.gif",
     deployedUrl: "https://tamarajoniec.github.io/Code-Refactor/",
     githubUrl: "https://github.com/TamaraJoniec/Code-Refactor",
   },
   {
     id: "3",
     title: "Furniture store",
-    imageUrl:
-      "https://github.com/TamaraJoniec/Furniture-Website/raw/main/images/Screenshot.png",
+    imageUrl: "https://github.com/TamaraJoniec/Furniture-Website/raw/main/images/Screenshot.png",
     deployedUrl: "https://tamarajoniec.github.io/Furniture-Website/",
     githubUrl: "https://github.com/TamaraJoniec/Furniture-Website",
   },
   {
     id: "4",
     title: "Password generator",
-    imageUrl:
-      "https://github.com/TamaraJoniec/Password-Generator/raw/main/assets/images/Screenshot.png",
+    imageUrl: "https://github.com/TamaraJoniec/Password-Generator/raw/main/assets/images/Screenshot.png",
     deployedUrl: "https://tamarajoniec.github.io/Password-Generator/",
     githubUrl: "https://github.com/TamaraJoniec/Password-Generator/tree/main",
   },
@@ -45,6 +40,13 @@ const projectsData = [
     imageUrl: "https://github.com/TamaraJoniec/JumpLinks/raw/main/src/demo.gif",
     deployedUrl: "https://jump-links.vercel.app/",
     githubUrl: "https://github.com/TamaraJoniec/JumpLinks/tree/main",
+  },
+  {
+    id: "6",
+    title: "Github User Finder",
+    imageUrl: "https://github.com/TamaraJoniec/github-user-finder/raw/main/assets/images/app-screenshot.png",
+    deployedUrl: "https://tranquil-gecko-2f76e4.netlify.app/",
+    githubUrl: "https://github.com/TamaraJoniec/github-user-finder.git",
   },
 ];
 
